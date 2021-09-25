a3241ebf7f909b804246b92404a720f6c44a314f
强 王 <seeman1978@163.com>
2021-09-25 22:57:38 +0800

    fgets

---
+++ fgets/fgets.cpp
@@ -0,0 +1,23 @@
+﻿// fgets.cpp: 定义应用程序的入口点。
+//
+
+#include "fgets.h"
+#include "apue.h"
+using namespace std;
+
+int main()
+{
+	char buf[MAXLINE];
+	while (fgets(buf, MAXLINE, stdin) != nullptr)
+	{
+		if (fputs(buf, stdout) == EOF)
+		{
+			err_sys("output error");
+		}
+	}
+	if (ferror(stdin))
+	{
+		err_sys("input error");
+	}
+	return 0;
+}
