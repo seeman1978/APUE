9d700907c693e57cd613a993655d86beb5372f89
强 王 <seeman1978@163.com>
2021-09-25 22:47:00 +0800

    getc

---
+++ getc/getc.cpp
@@ -0,0 +1,19 @@
+﻿// getc.cpp: 定义应用程序的入口点。
+//
+
+#include "getc.h"
+#include "apue.h"
+
+using namespace std;
+
+int main()
+{
+	int c;
+	while((c=getc(stdin)) != EOF){
+		if (putc(c, stdout) == EOF)
+		{
+			err_sys("output error");
+		}
+	}
+	return 0;
+}
