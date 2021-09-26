a7516c117014514b294e8cff6c1a17c4086abdad
qiang wang <qiang.wang@megatronix.co>
2021-09-26 10:50:55 +0800

    tmp file

--- TMP/main.cpp
+++ TMP/main.cpp
@@ -2,21 +2,28 @@
 #include "apue.h"
 int main() {
     char name[L_tmpnam]{"./indi_XXXXXX"}, line[MAXLINE];
-    FILE* fp;
+    FILE *fp1, *fp2;
     int tmpfd = mkstemp(name);
     if (tmpfd == -1){
         err_sys("mkstemp failed");
     }
-    unlink(name);
+    //unlink(name);
+    fp1 = fdopen(tmpfd, "a+");
+    fputs("one line of output\n", fp1);
+    rewind(fp1);
+    if (fgets(line, sizeof(line), fp1) == nullptr){
+        err_sys("fgets error");
+    }
+    fputs(line, stdout);
 
     std::cout << name << '\n';
-    fp = tmpfile();
-    if (fp == nullptr){
+    fp2 = tmpfile(); /// auto unlink.
+    if (fp2 == nullptr){
         err_sys("tmpfile error");
     }
-    fputs("one line of output\n", fp);
-    rewind(fp);
-    if (fgets(line, sizeof(line), fp) == nullptr){
+    fputs("one line of output\n", fp2);
+    rewind(fp2);
+    if (fgets(line, sizeof(line), fp2) == nullptr){
         err_sys("fgets error");
     }
     fputs(line, stdout);
