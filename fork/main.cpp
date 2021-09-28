0b6fe013482493fac57778e93cb976cd48dbf3b9
qiang wang <qiang.wang@megatronix.co>
2021-09-28 09:06:20 +0800

    fork

---
+++ fork/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unistd.h>
+#include "apue.h"
+
+int globvar = 6;
+char buf[] = "a write to stdout\n";
+
+int main() {
+    int var{88};
+    pid_t pid;
+    if (write(STDOUT_FILENO, buf, sizeof(buf)-1) != sizeof(buf)-1){
+        err_sys("write error");
+    }
+    std::cout << "before fork\n";
+
+    if ((pid = fork()) < 0){
+        err_sys("fork error");
+    }
+    else if (pid == 0){///child process
+        ++globvar;
+        ++var;
+    }
+    else{
+        sleep(2);
+    }
+    printf("pid=%ld, glob=%d, var=%d, child pid=%d\n", (long)getpid(), globvar, var, pid);
+    return 0;
+}
