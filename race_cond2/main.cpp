56db909dbc314424fbfa1dab4e714ce8ca2217f7
qiang wang <qiang.wang@megatronix.co>
2021-09-29 12:45:54 +0800

    race condition

---
+++ race_cond2/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unistd.h>
+#include "apue.h"
+
+void charatatime(const std::string& str){
+    setbuf(stdout, nullptr);/// set unbuffered
+    for (const auto c : str) {
+        putc(c, stdout);
+    }
+}
+
+int main() {
+    pid_t pid;
+    TELL_WAIT();
+    if ((pid = fork()) < 0){
+        err_sys("fork error");
+    }
+    else if (pid == 0){///parent
+        WAIT_PARENT();
+        charatatime("out put from child\n");
+    }
+    else {
+        charatatime("out put from parent\n");
+        TELL_CHILD(pid);
+    }
+    return 0;
+}
