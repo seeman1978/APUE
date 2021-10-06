c4f36bb7b06204473bdc53112f388b7497d54ff0
强 王 <seeman1978@163.com>
2021-10-06 23:05:03 +0800

    signal reentrance

---
+++ signal2/signal2.cpp
@@ -0,0 +1,34 @@
+﻿// signal2.cpp: 定义应用程序的入口点。
+//
+
+#include  "apue.h"
+#include <iostream>
+using namespace std;
+
+void my_alarm(int signo)
+{
+	struct passwd* rootptr;
+	cout << "in signal handler \n";
+	if (rootptr = getpwnam("root"); rootptr == nullptr)
+	{
+		err_sys("getpwname(root) error");
+	}
+	alarm(1);
+}
+
+int main()
+{
+	struct passwd* ptr;
+	signal(SIGALRM, my_alarm);
+	alarm(1);
+	for (;;) {
+		if (ptr = getpwnam("sar"); ptr == nullptr) {
+			err_sys("getpwnam error");
+		}
+		if (strcmp(ptr->pw_name, "sar") != 0) {
+			cout << "return value corrupted!, pw_name =" << ptr->pw_name << "\n";
+		}
+	}
+	
+	return 0;
+}
