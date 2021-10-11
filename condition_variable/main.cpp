f7c4ff470be444e1fd410c418842efa4389894a1
qiang wang <qiang.wang@megatronix.co>
2021-10-11 09:59:26 +0800

    condition variable

---
+++ condition_variable/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+struct msg{
+    struct msg* m_next;
+};
+
+struct msg* workq;
+
+pthread_cond_t qready = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t qlock = PTHREAD_MUTEX_INITIALIZER;
+
+void process_msg(){
+    struct msg *mp;
+    for (;;){
+        pthread_mutex_lock(&qlock);
+        while(workq == nullptr){
+            pthread_cond_wait(&qready, &qlock);
+            mp = workq;
+            workq = mp->m_next;
+            pthread_mutex_unlock(&qlock);
+        }
+    }
+}
+
+void enqueue_msg(struct msg* mp){
+    pthread_mutex_lock(&qlock);
+    mp->m_next = workq;
+    workq = mp;
+    pthread_mutex_unlock(&qlock);
+    pthread_cond_signal(&qready);
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
