bb73f718046984528a369983a30120fb30b72fb4
qiang wang <qiang.wang@megatronix.co>
2021-10-14 20:23:20 +0800

    multi thread daemon

--- daemonize/main.cpp
+++ daemonize/main.cpp
@@ -4,6 +4,36 @@
 #include <fcntl.h>
 #include <sys/syslog.h>
 
+sigset_t  mask;
+extern int already_running();
+
+void reread(){
+
+}
+
+void *thr_fn(void* arg){
+    int err, signo;
+    for (;;){
+        err = sigwait(&mask, &signo);
+        if (err != 0){
+            syslog(LOG_ERR, "sigwait failed");
+            exit(1);
+        }
+        switch (signo) {
+            case SIGHUP:
+                syslog(LOG_INFO, "Re-reading configuration file");
+                reread();
+                break;
+            case SIGTERM:
+                syslog(LOG_INFO, "get SIGTERM exiting");
+                exit(0);
+            default:
+                syslog(LOG_INFO, "unexpected signal %d\n", signo);
+        }
+    }
+    return nullptr;
+}
+
 void daemonize(const char* cmd){
     int fd0, fd1, fd2;
     pid_t pid;
@@ -65,7 +95,44 @@ void daemonize(const char* cmd){
     }
 }
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char** argv) {
+    int err;
+    pthread_t tid;
+    char* cmd;
+    struct sigaction sa;
+
+    if (cmd = strrchr(argv[0], '/'); cmd == nullptr){
+        cmd = argv[0];
+    }
+    else{
+        cmd++;
+    }
+    /// become a daemon
+    daemonize(cmd);
+
+    /// make sure only one copy of the daemon is running
+    if (already_running()){
+        syslog(LOG_ERR, "daemon already running");
+        exit(1);
+    }
+
+    ///restore SIGHUP default and block all signals
+    sa.sa_handler = SIG_DFL;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    if (sigaction(SIGHUP, &sa, nullptr) < 0){
+        err_quit("%s: can't restore SIGHUP default");
+    }
+    sigfillset(&mask);
+    if (err = pthread_sigmask(SIG_BLOCK, &mask, nullptr); err != 0){
+        err_exit(err, "SIG_BLOCK error");
+    }
+
+    ///create a thread to handle SIGHUP AND SIGTERM
+    if (err = pthread_create(&tid, nullptr, thr_fn, 0); err != 0){
+        err_exit(err, "can't create thread");
+    }
+
+    ///proceed with the rest of the daemon
     return 0;
 }
