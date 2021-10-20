b01afaf9cf9d6127818c80b5caa7d467c49b2ac8
wq <seeman1978@163.com>
2021-10-20 22:32:24 +0800

    fflush

---
+++ fflush/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "apue.h"
+int main() {
+    int c;
+    while((c = getchar()) != EOF){
+        if (isupper(c)){
+            c = tolower(c);
+        }
+        if (putchar(c) == EOF){
+            err_sys("output error");
+        }
+        if (c == '\n'){
+            fflush(stdout);
+        }
+    }
+    return 0;
+}
