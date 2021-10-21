3b231321b7a5ef9d30a9d03a11b3d46eb8d54035
qiang wang <qiang.wang@megatronix.co>
2021-10-21 09:52:45 +0800

    coprocess fgets prevent to be deadlock

--- coprocess3/main.cpp
+++ coprocess3/main.cpp
@@ -3,6 +3,15 @@
 int main() {
     int int1, int2;
     char line[MAXLINE];
+
+    ///if there are not these four lines code, the program will be deadlock
+    if (setvbuf(stdin, nullptr, _IOLBF, 0) != 0){
+        err_sys("setvbuf error");
+    }
+    if (setvbuf(stdout, nullptr, _IOLBF, 0) != 0){
+        err_sys("setvbuf error");
+    }
+
     while (fgets(line, MAXLINE, stdin) != nullptr){
         if (sscanf(line, "%d%d", &int1, &int2) == 2){
             if (printf(":%d\n", int1+int2) == EOF){
